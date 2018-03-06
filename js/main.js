266158125f0e12b43fcda43e47c3dd9869efc15a
chrish88 <crehall88@gmail.com>
2018-03-06 07:24:57 -0800

    Added jquery to portfolio section, loads target div into portfolio section and hides all other genre divs

--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
-//var declarations
+//declarations
 const $portNav = $('.port-nav');
 const $slider1 = $('.slider-1');
 const $slider2 = $('.slider-2');
 const $slider3 = $('.slider-3');
 const $slider4 = $('.slider-4');
 
+
+//array that holds reference to every li
+const $portNavList = $('.port-list');
+//array that holds reference to every slider
+const $allSliders = $('.slide');
+
+
+
+//hide all slides
+$allSliders.hide();
+
 //listen for click on port nav
 $portNav.on('click', function(event){
+    //create var and set it event.target
     let $clickedLi = $(event.target);
-    console.log($clickedLi.attr('class'));
-    //if $clickedLi class name ===portraits
-    if ($clickedLi.attr('class') === 'port-list portraits'){
-        //display potraits div
-        $slider1.show();
-        
-    }else if ($clickedLi.attr('class') === 'port-list longExposures'){
-        //display potraits div
-        $slider2.show();
-    }else if ($clickedLi.attr('class') === 'port-list landScapes'){
-        //display potraits div
-        $slider3.show();
-    }else if ($clickedLi.attr('class') === 'port-list urban'){
-        //display potraits div
-        $slider4.show();
-    }
+    //loop through portNavList and return index and element
+    $portNavList.each(function(i, element){
+      //if clickedLi is equal to element li, display matching slide div
+        if ($clickedLi.is($(element))){
+            //create var and set it equal to current slide index
+            $allSliders.hide();
+            let currentSlide = $($allSliders[i]);
+            currentSlide.fadeIn(1500);
+            
+            
+        }
+    });
 
-    
+
 });
+
